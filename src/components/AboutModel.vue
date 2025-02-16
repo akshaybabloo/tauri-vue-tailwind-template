13ca0ba2131d63667b740d9acd1ede630255b7c2
Akshay Raj Gollahalli <akshay@gollahalli.com>
2025-02-17 02:46:36 +1300

    update model

--- src/components/AboutModel.vue
+++ src/components/AboutModel.vue
@@ -12,28 +12,16 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div v-if="model" class="bg-base-100/50 dark:bg-base-100/80 fixed inset-0 z-40">
-    <div
-      class="flex overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
-    >
-      <div class="relative p-4 w-full max-w-md max-h-full">
-        <div class="relative bg-base-100 rounded-lg shadow-sm dark:bg-base-100 border border-base-300">
-          <button
-            type="button"
-            class="absolute top-3 end-2.5 text-base-content bg-transparent hover:bg-base-200 hover:text-base-content rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-base-200 dark:hover:text-base-content"
-            data-modal-hide="popup-modal"
-            @click="model = false"
-          >
-            <span><PhX size="20" /></span>
-            <span class="sr-only">Close modal</span>
-          </button>
-          <div class="p-4 md:p-5 text-center">
-            <img src="../assets/logo.png" class="mx-auto mb-4 w-12 h-12" alt="Logo" />
-            <h3 class="mb-5 text-lg font-normal text-base-content dark:text-base-content">About</h3>
-            <p>{{ version }}</p>
-          </div>
-        </div>
-      </div>
+<dialog id="my_modal_5" class="modal modal-bottom sm:modal-middle" :class="{ 'modal-open': model }">
+  <div class="modal-box border border-base-300">
+    <img src="../assets/logo.png" class="mx-auto mb-4 w-12 h-12" alt="Logo" />
+    <h3 class="text-lg font-bold text-center">tauri-vue-tailwind-template</h3>
+    <p class="py-4 text-center">Version: {{version}}</p>
+    <div class="modal-action">
+      <form method="dialog">
+        <button class="btn" @click="model=false">Close</button>
+      </form>
     </div>
   </div>
+</dialog>
 </template>
