541591552aa16a5488e13b619bba20d103b13dec
Akshay Raj Gollahalli <akshay@gollahalli.com>
2025-02-20 14:38:12 +1300

    Use correct icons for window controller

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import FooterBar from "./components/FooterBar.vue";
     <header class="fixed top-0 left-0 right-0 z-50 bg-base-100">
       <Nav />
     </header>
-    <main class="flex-1 mt-7 mb-7 bg-base-200">
+    <main class="flex-1 mt-9 mb-7 bg-base-200">
         <p>Your code here</p>
     </main>
     <footer class="fixed bottom-0 left-0 right-0 bg-base-100">
